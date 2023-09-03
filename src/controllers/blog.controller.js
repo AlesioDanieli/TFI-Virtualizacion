2798abfae29f72a3515411e44cf0ca4459526a57
AlesioDanieli <71045796+AlesioDanieli@users.noreply.github.com>
2023-09-03 20:42:32 -0300

    Cambiar SELECT de Blogs

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -1,6 +1,6 @@
 import { pool } from '../db.js'
 
 export const blog = async (req, res) => {
-    const [result] = await pool.query('SELECT descripcion FROM blog')
+    const [result] = await pool.query('SELECT * FROM blog')
     res.json(result)
 };
